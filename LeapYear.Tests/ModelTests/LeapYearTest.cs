c5d5c5baac93e936f47515c603afca23436b83b0
Andy Grossberg and Cameron Anderson <andy@>
2018-02-12 10:09:03 -0800

    add LeapYearTests

--- LeapYear.Tests/ModelTests/LeapYearTest.cs
+++ LeapYear.Tests/ModelTests/LeapYearTest.cs
@@ -9,7 +9,44 @@ namespace LeapYear.Tests
     [TestMethod]
     public void IsLeapYear_NumberDivisibleByFour_True()
     {
-      // eventually your testing code will go here
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsDivisibleByFour(2012));
+      Assert.AreEqual(true, testLeapYear.IsDivisibleByFour(2012));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_NumberNotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsDivisibleByFour(1999));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByOneHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsDivisibleByOneHundred(2100));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsDivisibleByOneHundred(2004));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsDivisibleByFourHundred(2400));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFourHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsDivisibleByFourHundred(2004));
     }
   }
 }
